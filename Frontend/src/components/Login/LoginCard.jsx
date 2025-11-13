b5c33c793f9030d7a911697ce1a884fade66810a
PurePeter <phatlenguyenthanh4@gmail.com>
2025-11-13 18:24:58 +0700

    fix little bit

--- Frontend/src/components/Login/LoginCard.jsx
+++ Frontend/src/components/Login/LoginCard.jsx
@@ -251,11 +251,7 @@ function LoginCard() {
             formState === "login"
               ? "opacity-100 translate-x-0"
               : "opacity-0 -translate-x-full pointer-events-none invisible"
-          }`}
-          className={`absolute w-full top-0 left-0 transition-all duration-500 ease-in-out ${isLogin
-              ? "opacity-100 translate-x-0"
-              : "opacity-0 -translate-x-full pointer-events-none"
-            }`} // Gán ref cho div chứa form Login
+          }`} // Gán ref cho div chứa form Login
         >
           {/* Login Form */}
           <form
